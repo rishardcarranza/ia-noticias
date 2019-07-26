3fc2bf5a12e33b29b4eedace753d51d81978e628
Richard Carranza <rishardcarranza@gmail.com>
2019-07-25 22:23:24 -0600

    Added service, modified tabs structure, create interfaces

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -10,6 +10,7 @@ export class NoticiasService {
   constructor(private http:HttpClient) { }
 
     getTopHeadLines() {
+        // tslint:disable-next-line: max-line-length
         return this.http.get<TopHeadLines>(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f462be3c9e0949ed839e85474cf2205a`);
     }
 
