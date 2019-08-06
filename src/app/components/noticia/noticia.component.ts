03e8b70e28ba2d36ea02def8e65ce2b2c0dafad3
Richard Carranza <rishardcarranza@gmail.com>
2019-08-06 00:27:21 -0600

    Added action sheet to share news

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -13,13 +15,51 @@ export class NoticiaComponent implements OnInit {
     @Input() noticia: Article;
     @Input() indice: number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser,
+              private actionSheetCtrl: ActionSheetController,
+              private socialSharing: SocialSharing) { }
 
   ngOnInit() {}
 
   abrirNoticia() {
     //   console.log('Noticia', this.noticia.url);
       const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+        buttons: [{
+          text: 'Compartir',
+          icon: 'share',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Share clicked');
+            this.socialSharing.share(
+                this.noticia.title,
+                this.noticia.source.name,
+                '',
+                this.noticia.url
+            );
+          }
+        }, {
+          text: 'Favorito',
+          icon: 'heart',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Cancelar',
+          icon: 'close',
+          cssClass: 'action-dark',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }]
+      });
+
+    await actionSheet.present();
   }
 
 }
